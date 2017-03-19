f8e5e588a50b162020a2545f3f69af0f6f1920b0
sergg_grom <sergg.grom@gmail.com>
2017-03-19 20:14:57 +0300

    next steps on 2 chapter

--- DM_1_FirstExamples.py
+++ DM_1_FirstExamples.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 from collections import Counter
 from pandas import DataFrame, Series
 import pandas as pd
@@ -51,33 +53,74 @@ tz_counts = frame['tz'].value_counts()
 clean_tz = frame['tz'].fillna('Missing')
 clean_tz[clean_tz == ''] = 'Unknown'
 tz_counts = clean_tz.value_counts()
-print(tz_counts[:10])
+#print(tz_counts[:10])
 
 tz_counts[:10].plot(kind='barh', rot=0)
 
+#print(frame['a'][1])
+#print(frame['a'][51])
+
+results = Series([x.split()[0] for x in frame.a.dropna()])
+
+#print(results[:5])
+
+#print(results.value_counts()[:8])
+
+cframe = frame[frame.a.notnull()]
+
+operating_system = np.where(cframe['a'].str.contains('Windows'),
+                            'Windows', 'Not Windows')
+#print(operating_system[:5])
 
 
+by_tz_os = cframe.groupby(['tz', operating_system])
+
+agg_counts = by_tz_os.size().unstack().fillna(0)
+
+#print(agg_counts[:10])
+
+indexer = agg_counts.sum(1).argsort()
+
+#print(indexer[:10])
+
+count_subset = agg_counts.take(indexer)[-10:]
+
+#print(count_subset)
+
+#count_subset.plot(kind='barh', stacked=True)
+#plt.figure()
+
+normed_subset = count_subset.div(count_subset.sum(1), axis=0)
+#normed_subset.plot(kind='barh', stacked=True)
 
 
+######################MOVIELENS 1 M################################################
 
+encoding = 'latin1'
+upath = 'users.txt'
+rpath = 'ratings.txt'
+mpath = 'movies.txt'
 
+unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
+mnames = ['movie_id', 'title', 'genres']
 
+users = pd.read_table('users.txt', sep='::', header=None, names=unames)
 
+ratings = pd.read_table('ratings.txt', sep='::', header=None, names=rnames)
 
+movies = pd.read_table('movies.txt', sep='::', header=None, names=mnames)
 
+#print(users[:5])
+#print(ratings[:5])
+#print(movies[:5])
 
+#print(ratings.info())
 
+data = pd.merge(pd.merge(ratings, users), movies)
+#print(data.info())
 
-
-
-
-
-
-
-
-
-
-
+print(data.ix[0])
 
 
 
