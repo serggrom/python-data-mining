d274fc4ce1cb696a644a324e814a0b567253dd45
sergg_grom <sergg.grom@gmail.com>
2017-03-24 11:21:36 +0300

    end of NumPy chapter

--- DM_4_NP.py
+++ DM_4_NP.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import randn
+from numpy.linalg import inv, qr
+from random import normalvariate
+import random
 
 data = [6, 7.5, 8, 0, 1]
 arr = np.array(data)
@@ -237,6 +240,151 @@ for i in range(n):
     else:
         result.append(3)
 '''
+
+
+arr11 = np.random.randn(5, 4)
+
+#print(arr10.mean())
+#print(np.mean(arr10))
+
+
+#print(arr11.sum())
+#print(arr11)
+
+#print(arr11.mean(axis=1))
+#print(arr11.sum(0))
+
+
+arrays = np.array([[0, 1, 2], [3, 4, 5], [7, 8, 9]])
+
+#print(arrays.cumsum())
+
+#print(arrays.cumprod(1))
+
+arr12 = randn(100)
+
+pos = (arr12 > 0).sum()
+
+#print(pos)
+
+bools = np.array([False, False, True, False])
+
+#print(bools.any())
+#print(bools.all())
+
+
+arr13 = randn(8)
+
+#print(arr13)
+arr13.sort()
+
+arr13 = randn(5, 3)
+
+#print(arr13)
+
+arr13.sort(1)
+
+#print(arr13)
+
+large_arr = randn(1000)
+large_arr.sort()
+
+quantil = large_arr[int(0.05 * len(large_arr))]
+
+#print(quantil)
+
+
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Joe', 'Joe'])
+
+#print(np.unique(names))
+
+ints = np.array([3, 3, 3, 2, 2, 1, 1, 4, 4])
+
+#print(np.unique(ints))
+
+#print(sorted(set(names)))
+
+
+values = np.array([6, 0, 0, 3, 2, 5, 6])
+
+srt = np.in1d(values, [2, 3, 6])
+
+#print(srt)
+
+
+arr14 = np.arange(10)
+
+np.save('some_array', arr14)
+
+#print(np.load('some_array.npy'))
+
+np.savez('array_archive.npz', a=arr14, b=arr14)
+
+arch = np.load('array_archive.npz')
+
+#print(arch['b'])
+
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+
+y = np.array([[6., 23.], [-1., 7.], [8, 9]])
+
+z = x.dot(y)
+
+#print(z)
+
+one = np.ones(3)
+
+z = np.dot(x, one)
+
+#print(z)
+
+X = randn(5, 5)
+
+mat = X.T.dot(X)
+
+#print(inv(X))
+
+#print(mat.dot(inv(mat)))
+
+q, r = qr(mat)
+
+#print(r)
+
+
+samples = np.random.normal(size=(4, 4))
+x_samples = randn(1000)
+
+#print(samples)
+
+N = 1000000
+#samples.reshape(0, 16)
+
+#%timeit samples = normalvariate(0, 1) for _ in xrange(N)
+
+#plt.plot(x_samples)
+#plt.show()
+
+#np.random.seed(12345)
+
+position = 0
+walk = [position]
+
+steps = 1000
+
+for i in range(steps):
+    step = 1 if random.randint(0, 1) else -1
+    position += step
+    walk.append(position)
+
+#plt.plot(walk)
+#plt.show()
+
+
+
+
+
+
+
 
 
 
